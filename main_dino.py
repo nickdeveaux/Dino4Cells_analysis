2846e66be1407f4da23ea2bb879a0faf3e5a9b2e
Nick DV <nick@andrena.com>
2023-11-21 12:09:28 -0500

    removing list flattening

--- main_dino.py
+++ main_dino.py
@@ -580,9 +580,7 @@ def train_one_epoch(
             param_group["lr"] = lr_schedule[it]
             if i == 0:  # only the first group is regularized
                 param_group["weight_decay"] = wd_schedule[it]
-
-        # flatten images into a single list (due to one_channel loader)
-        images = [item for sublist in images for item in (sublist if isinstance(sublist, list) else [sublist])]
+    
         # move images to gpu
         images = [im.cuda(non_blocking=True) for im in  images]
         # teacher and student forward passes + compute dino loss
