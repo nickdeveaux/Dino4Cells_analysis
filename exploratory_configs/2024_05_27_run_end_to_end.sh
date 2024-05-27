e723d9f9da549c5a7725225af38dd358e0dabdc8
Nick on Columbia Infra <nrdeveaux@gmail.com>
2024-05-27 15:10:20 -0400

    more attempts to get dino to run

--- exploratory_configs/2024_05_27_run_end_to_end.sh
+++ exploratory_configs/2024_05_27_run_end_to_end.sh
@@ -2,4 +2,4 @@
 source ~/miniconda3/bin/activate
 conda activate dino4cells
 export CUDA_VISIBLE_DEVICES=0
-CUDA_VISIBLE_DEVICES=0 python /mnt/vast/hpc/homes/ndv2107/Dino4Cells_analysis/run_end_to_end.py --config /mnt/vast/hpc/homes/ndv2107/Dino4Cells_analysis/exploratory_configs/2024_05_27_config_HPA_FOV_rerun_end_to_end.yaml 2>&1 | tee log.txt
+CUDA_VISIBLE_DEVICES=0 python /mnt/vast/hpc/homes/ndv2107/Dino4Cells_analysis/run_dino.py --config /mnt/vast/hpc/homes/ndv2107/Dino4Cells_analysis/exploratory_configs/2024_05_27_config_HPA_FOV_rerun.yaml 2>&1 | tee log.txt
